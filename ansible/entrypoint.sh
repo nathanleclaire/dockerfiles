c9bd1268f8923d8684541a6d2cf653c19e8a8aeb
Nathan LeClaire <nathan.leclaire@gmail.com>
2015-12-11 18:39:52 -0800

    Make sure python installed before bootstrap
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -12,6 +12,7 @@ ssh-keygen -f /root/.ssh/id_rsa -P ""
 cp /hostssh/authorized_keys /hostssh/authorized_keys.bak
 cat /root/.ssh/id_rsa.pub >>/hostssh/authorized_keys
 
+ansible all -i "localhost," -m raw -a "apt-get install -y python-minimal"
 ansible-playbook -i "localhost," "$@"
 
 mv /hostssh/authorized_keys.bak /hostssh/authorized_keys
